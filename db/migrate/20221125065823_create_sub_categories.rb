203872aeb3da1871280d176f3156ea929f552a2b
Imran Akhtar <imran.akhtar@brainxtech.com>
2022-11-25 16:16:15 +0500

    Sub category setup

--- db/migrate/20221125065823_create_sub_categories.rb
+++ db/migrate/20221125065823_create_sub_categories.rb
@@ -1,8 +1,8 @@
 class CreateSubCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :sub_categories do |t|
-      t.belongs_to :categories
-      t.belongs_to :products
+      t.belongs_to :category
+      t.belongs_to :product
       t.string :name
       t.string :description
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 Category.create(name: "Men's")
 Category.create(name: "women's")
 Product.create(name: "Nike Retro", description: "Created for the hardwood but taken to the streets, the '80s b-ball icon returns with perfectly sheened overlays and original university colors. With its classic hoops design, the Nike Dunk High Retro channels '80s vintage back onto the streets while its padded, high-top collar adds an old-school look rooted in comfort.", price: 120.5)
-SubCategory.create(name: "Shoes", description: "80s b-ball icon returns with perfectly")
+SubCategory.create(category_id: 1, product_id: 1, name: "Shoes", description: "80s b-ball icon returns with perfectly")
 
