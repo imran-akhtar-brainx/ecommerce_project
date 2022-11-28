599e77d764cae1ffb06083253a469fa384cddff5
Imran Akhtar <imran.akhtar@brainxtech.com>
2022-11-29 01:20:13 +0500

    view improved

--- app/javascript/controllers/cart_controller.js
+++ app/javascript/controllers/cart_controller.js
@@ -4,7 +4,8 @@ export default class extends Controller {
     static targets = [ "myspan" ]
     static values = {
         url: String,
-        product: Number
+        product: Number,
+
     }
 
     connect() {
@@ -12,11 +13,23 @@ export default class extends Controller {
     }
 
     addToCart(event){
-        let product_id = event.currentTarget.getAttribute("data-product");
+        let product_id = event.currentTarget.getAttribute("data-product-id");
+        let product_name = event.currentTarget.getAttribute("data-product-name");
+        let product_price = event.currentTarget.getAttribute("data-product-price");
+        const element = document.createElement("div");
+        const header = document.createElement("h4");
+        header.innerText = product_name;
+        const paragh = document.createElement("p");
+        paragh.innerText = `Price ${product_price}$ `
+        element.appendChild(header);
+        element.appendChild(paragh);
+        const divElement = document.getElementById('cart');
+        element.classList.add("border")
+        divElement.appendChild(element);
+
         this.products_ids.push(product_id);
         // sessionStorage.setItem("product_ids", this.products_ids);
         document.getElementById('products_ids').value = this.products_ids;
-        return this.products_ids
     }
 
     checkout(){
