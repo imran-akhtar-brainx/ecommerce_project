69a45a93e9accf83e59858683d0a1dff405560c8
Imran Akhtar <imran.akhtar@brainxtech.com>
2022-11-29 12:55:43 +0500

    PR fixes

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -2,7 +2,6 @@ class SubCategoriesController < ApplicationController
 
   def index
     @sub_categories = SubCategory.where(category_id: params[:id].present? ? params[:id] : Category.first.id)
-    # @sub_categories = @sub_categories.where(category_id: params[:id]) if params[:id].present?
   end
 
 end
